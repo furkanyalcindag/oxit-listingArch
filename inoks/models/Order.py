ac24f1bec5d5b3a5e1524a114bbf80a2dd03d07c
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2020-03-20 10:24:15 +0300

    User dahsboard ağaç cirosu eklendi

--- inoks/models/Order.py
+++ inoks/models/Order.py
@@ -41,7 +41,6 @@ class Order(models.Model):
     def latest_catch(self):
         if len(self.order_situations.all())>0:
             return self.order_situations.all()[0].name
-
             #return self.order_situations.get_queryset()[len(self.order_situations.get_queryset())-1].name
             #return self.order_situations.all().order_by('id')[len(self.order_situations.all())-1]
         else:
