6ad220627cbc60723803ea4ccbc562cdd05d4fd8
doganbrc95@gmail.com <asdasvas17>
2020-10-21 11:48:23 +0300

    abone kayıt etme , listeye ürün ekleme/görüntüleme ,profil adı ekleme

--- oxiterp/settings/dev.py
+++ oxiterp/settings/dev.py
@@ -6,7 +6,7 @@ from oxiterp.settings.base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Oxit',
+        'NAME': 'listArch',
         'USER': 'postgres',
         'PASSWORD': 'oxit2016',
         'HOST': 'localhost',
